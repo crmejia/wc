4e0a0399db8da406f4809a1b9253ce3f1a7eca85
Crismar Mejia <crismar.mejia@gmail.com>
2025-02-15 20:27:21 -0400

    actual step 5, I skipped 4

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use core::str;
-use std::{env, fs, path::Path, str::Chars};
+use std::{env, fs, path::Path};
 
 fn count_bytes(input: Vec<u8>) -> u32 {
     input.len() as u32
@@ -31,13 +31,15 @@ fn count_locale_chars(input: Vec<u8>) -> Result<u32, &'static str> {
 
     Ok(input.chars().count() as u32)
 }
-pub fn run() -> Result<u32, &'static str> {
-    let config = match Config::build(env::args()) {
+pub fn run() -> Result<String, &'static str> {
+    //this is setup, not part of the run
+    let config = match Config::build(env::args().collect()) {
         Ok(config) => config,
         Err(e) => return Err(e),
     };
 
     //open file
+    //todo this could actually be injected as a Vec<u8> so it can be tested
     let path = Path::new(config.file_path.as_str());
     let file = match fs::read(path) {
         Ok(file) => file,
@@ -45,19 +47,32 @@ pub fn run() -> Result<u32, &'static str> {
     };
 
     match config.option.as_str() {
-        "-c" => Ok(count_bytes(file)),
+        "-c" => Ok(format!("{}", count_bytes(file))),
         "-l" => match count_lines(file) {
-            Ok(number_of_lines) => Ok(number_of_lines),
+            Ok(number_of_lines) => Ok(format!("{}", number_of_lines)),
             Err(e) => Err(e),
         },
         "-w" => match count_words(file) {
-            Ok(number_of_words) => Ok(number_of_words),
+            Ok(number_of_words) => Ok(format!("{}", number_of_words)),
             Err(e) => Err(e),
         },
         "-m" => match count_locale_chars(file) {
-            Ok(number_of_chars) => Ok(number_of_chars),
+            Ok(number_of_chars) => Ok(format!("{}", number_of_chars)),
             Err(e) => Err(e),
         },
+        "*" => {
+            //ineffiecient cloning thanks compiler
+            let lines = count_lines(file.clone()).unwrap();
+            let words = count_words(file.clone()).unwrap();
+            let filename = path.file_name().unwrap().to_str().unwrap();
+            Ok(format!(
+                "{} {} {} {}",
+                lines,
+                words,
+                count_bytes(file),
+                filename
+            ))
+        }
         _ => Err("unknown option"),
     }
 }
@@ -68,26 +83,49 @@ pub struct Config {
     pub option: String,
 }
 impl Config {
-    pub fn build(input: impl IntoIterator<Item = String>) -> Result<Config, &'static str> {
-        let mut args = input.into_iter();
-        args.next(); //skipprogram name
-        let option = match args.next() {
-            Some(arg) => match arg.as_str() {
-                "-c" => arg,
-                "-l" => arg,
-                "-w" => arg,
-                "-m" => arg,
-                _ => return Err("unknown option"),
-            },
-            None => return Err("Not enough arguments"),
-        };
+    // pub fn build(input: impl IntoIterator<Item = String>) -> Result<Config, &'static str> {
+    pub fn build(args: Vec<String>) -> Result<Config, &'static str> {
+        // let mut args = input.into_iter();
+        // args.
+        // args.next(); //skipprogram name
 
-        let file_path = match args.next() {
-            Some(arg) => arg,
-            None => return Err("not enough arguments"),
-        };
+        // let option = match args.next() {
+        //     Some(arg) => match arg.as_str() {
+        //         "-c" => arg,
+        //         "-l" => arg,
+        //         "-w" => arg,
+        //         "-m" => arg,
+        //         _ => return Err("unknown option"),
+        //     },
+        //     None => return Err("Not enough arguments"),
+        // };
 
-        Ok(Config { file_path, option })
+        // let file_path = match args.next() {
+        //     Some(arg) => arg,
+        //     None => return Err("not enough arguments"),
+        // };
+
+        // Ok(Config { file_path, option })
+        match args.len() {
+            1 => todo!(), //check stdin
+            2 => {
+                //check if its option or file name
+                let arg = args[1].clone();
+                if arg.starts_with("-") {
+                    todo!() //use option + stdin
+                }
+
+                Ok(Config {
+                    file_path: arg,
+                    option: "*".to_string(),
+                })
+            }
+            3 => Ok(Config {
+                file_path: args[2].clone(),
+                option: args[1].clone(),
+            }),
+            _ => return Err("too many arguments"),
+        }
     }
 }
 
@@ -138,6 +176,8 @@ mod tests {
             Err(_) => assert!(false, "should not fail"),
         }
     }
+
+    //input Config build tests
     #[test]
     fn test_input_wrong_option() {
         let input: Vec<String> = vec![
@@ -146,8 +186,8 @@ mod tests {
             "/hello/file.txt".to_string(),
         ];
         match Config::build(input) {
-            Ok(_) => assert!(false, "should  fail"),
-            Err(_) => (),
+            Ok(_) => (),
+            Err(_) => assert!(false, "should not fail"),
         };
     }
     #[test]
@@ -238,5 +278,20 @@ mod tests {
             Ok(_) => assert!(false, "should fail"),
             Err(_) => (),
         };
+    }
+
+    #[test]
+    fn test_input_no_option_filename() {
+        let want = Config {
+            file_path: "/hello/file.txt".to_string(),
+            option: "*".to_string(),
+        };
+
+        let input: Vec<String> = vec!["ccwc".to_string(), "/hello/file.txt".to_string()];
+
+        match Config::build(input) {
+            Ok(got) => assert_eq!(want, got),
+            Err(_) => assert!(false, "should not fail"),
+        }
     }
 }
